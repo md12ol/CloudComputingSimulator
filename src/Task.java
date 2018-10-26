892e1f38710b1dd1cd4ff250bbd79f1e77572f5b
Michael Dube <md12ol@brocku.ca>
2018-10-25 20:01:24 -0400

    Made some progress on Task

--- src/Task.java
+++ src/Task.java
@@ -6,36 +6,39 @@
 public class Task {
 
   // Variables regardless of location of processing
-  double inputData;         // Input Data Size
-  double outputData;        // Output Data Size
-  String app;               // App Type TODO: May need to change how app type is stored
-  double totalTime;         // Total time used to process task
-  double totalEnergy;       // Total energy used by mobile device to process task
-  boolean calculated;       // If the energy use and processing time of the task has been calculated
+  private double inputData;         // Input Data Size
+  private double outputData;        // Output Data Size
+  private double cyclesPerByte;     // The number of CPU cycles necessary per byte of input data
+  private double totalTime;         // Total time used to process task
+  private double totalEnergy;       // Total energy used by mobile device to process task
+  private boolean calculated;       // If the energy use and processing time of the task has been calculated
 
   // Locally (L)
-  boolean compL;            // Task is processed locally
-  double energyUseL;        // Energy used for processing locally
-  double processTimeL;      // Time to process if processed locally
+  private boolean compL;            // Task is processed locally
+  private double energyUseL;        // Energy used for processing locally
+  private double processTimeL;      // Time to process if processed locally
 
   // Access Point (AP)
-  boolean compAP;          // Task is processed on CAP
-  double energyTransAP;    // Energy consumed for transmission to CAP
-  double energyReceivAP;   // Energy consumed for receiving from CAP
-  double upTimeAP;         // Time to upload to CAP
-  double downTimeAP;       // Time to download from CAP
-  double processTimeAP;    // Time to process if processed on CAP
+  private boolean compAP;          // Task is processed on CAP
+  private double energyTransAP;    // Energy consumed for transmission to CAP
+  private double energyReceivAP;   // Energy consumed for receiving from CAP
+  private double upTimeAP;         // Time to upload to CAP
+  private double downTimeAP;       // Time to download from CAP
+  private double processTimeAP;    // Time to process if processed on CAP
 
   // Remote Cloud (RC)
-  boolean compRC;         // Task is processed on RC
-  double transTimeRC;     // Time to transmit to RC
-  double processTimeRC;   // Time to process if processed on RC
+  private boolean compRC;         // Task is processed on RC
+  private double transTimeRC;     // Time to transmit to RC
+  private double processTimeRC;   // Time to process if processed on RC
 
   // Costs
-  double costAP;          // Cost of letting AP process task
-  double costRC;          // Cost of letting RC process task
+  private double costAP;          // Cost of letting AP process task
+  private double costRC;          // Cost of letting RC process task
 
-  public Task() {
+  public Task(double inputData, double outputData, double cyclesPerByte) {
+    this.inputData = inputData;
+    this.outputData = outputData;
+    this.cyclesPerByte = cyclesPerByte;
     calculated = false;
   } // Constructor
 
@@ -43,6 +46,10 @@ public class Task {
     compL = true;
     compAP = false;
     compRC = false;
+  }
+
+  public void markComplete() {
+    // Make sure that this is the end of the
   }
 
 }
